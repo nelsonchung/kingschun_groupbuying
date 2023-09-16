213b67b41757e74147d9602ae48c17b4b024660a
Nelson Chung <chihchun.chung@gmail.com>
2023-09-16 15:42:56 +0800

    加好AddPage的layout

--- groupbuying_manager/lib/add_product.dart
+++ groupbuying_manager/lib/add_product.dart
@@ -13,9 +13,9 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  * Author: Nelson Chung
- * Creation Date: August 28, 2023
+ * Creation Date: September 16, 2023
  */
- 
+
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'database_helper.dart';
@@ -23,220 +23,239 @@ import 'package:flutter/cupertino.dart';
 import 'dart:io';
 
 class Product {
-  final String? name;
-  final String? description;
-  final int price;
-  final int quantity;
-  final String? photo;
-  final String? shop;
+	final String? name;
+	final String? description;
+	final int price;
+	final int quantity;
+	final String? photo;
+	final String? shop;
 
-  Product({
-    this.name,
-    this.description,
-    required this.price,
-    required this.quantity,
-    this.photo,
-    this.shop,
-  });
+	Product({
+			this.name,
+			this.description,
+			required this.price,
+			required this.quantity,
+			this.photo,
+			this.shop,
+			});
 
-  Map<String, dynamic> toMap() {
-    return {
-      'name': name,
-      'description': description,
-      'price': price,
-      'quantity': quantity,
-      'photo': photo,
-      'shop': shop,
-    };
-  }
+	Map<String, dynamic> toMap() {
+		return {
+			'name': name,
+				'description': description,
+				'price': price,
+				'quantity': quantity,
+				'photo': photo,
+				'shop': shop,
+		};
+	}
 }
 
-class AddProductScreen extends StatefulWidget {
-  @override
-  _AddProductScreenState createState() => _AddProductScreenState();
+class AddProductPgae extends StatefulWidget {
+	@override
+		_AddProductPageState createState() => _AddProductPageState();
 }
 
-class _AddProductScreenState extends State<AddProductScreen> {
-  String? productName;
-  String? productDescription;
-  String? selectedPhoto;
-  //List<String> priceDigits = ['0', '0', '0', '0']; // 千, 百, 十, 個
-  String? quantityDigit = '0';
-  String? selectedShop = "KingsChun";
-  int selectedPrice = 0;
-  int selectedQuantity = 0;
+class _AddProductPageState extends State<AddProductPgae> {
+	File? _image;
+	final picker = ImagePicker();
+	String dropdownValue = 'macbook pro';
 
-/*
-  double get price {
-    int priceValue = int.parse(priceDigits.join());
-    return priceValue.toDouble();
-  }
-*/
-/*
-  double get quantity {
-    return double.parse(quantityDigit ?? '0');
-  }
-*/
+	Future getImage(bool isCamera) async {
+		final pickedFile = await picker.pickImage(
+				source: isCamera ? ImageSource.camera : ImageSource.gallery);
 
-  final picker = ImagePicker();
+		setState(() {
+				if (pickedFile != null) {
+				_image = File(pickedFile.path);
+				} else {
+				print('No image selected.');
+				}
+				});
+	}
 
-  Future<void> _addProductToDb() async {
-    final product = Product(
-      name: productName,
-      description: productDescription,
-      price: selectedPrice,
-      quantity: selectedQuantity,
-      photo: selectedPhoto,
-      shop: selectedShop,
-    );
-
-    final db = DatabaseHelper.instance;
-    final result = await db.insert(product.toMap());
-
-    if (result > 0) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('新增商品成功!')),
-      );
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('新增商品失敗!')),
-      );
-    }
-  }
-
-  Future<void> _pickImage() async {
-    showModalBottomSheet(
-      context: context,
-      builder: (context) => Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          ListTile(
-            leading: Icon(Icons.photo_camera),
-            title: Text('拍照'),
-            onTap: () async {
-              final pickedFile = await picker.pickImage(source: ImageSource.camera);
-              if (pickedFile != null) {
-                setState(() {
-                  selectedPhoto = pickedFile.path;
-                });
-              }
-              Navigator.pop(context);
-            },
+	@override
+		Widget build(BuildContext context) {
+			return Scaffold(
+					backgroundColor: Color(0xFF4CAD73),
+					appBar: AppBar(
+						backgroundColor: Color(0xFF4CAD73),
+						title: Text('File Upload App'),
+						),
+					body: Padding(
+						padding: const EdgeInsets.all(16.0),
+						child: SingleChildScrollView(
+							child: Column(
+								children: <Widget>[
+								// 拍照與選擇圖片按鈕
+								Row(
+									mainAxisAlignment: MainAxisAlignment.spaceAround,
+									children: [
+									ElevatedButton(
+										style: ElevatedButton.styleFrom(primary: Color(0xFF4CAD73)),
+										child: Text('拍照', style: TextStyle(fontSize: 20)),
+										onPressed: () {
+										getImage(true);
+										},
+										),
+									ElevatedButton(
+										style: ElevatedButton.styleFrom(primary: Color(0xFF4CAD73)),
+										child: Text('選擇圖片', style: TextStyle(fontSize: 20)),
+										onPressed: () {
+										getImage(false);
+										},
+										),
+									],
+								   ),
+					SizedBox(height: 16),
+					_image == null
+						? Text('No image selected.')
+						: Image.file(_image!, height: 200, width: 200),
+					SizedBox(height: 16),
+          const Divider(
+            //height: 20,
+            thickness: 5,
+            //indent: 20,
+            endIndent: 0,
+            color: Color(0xFF4CAD25),
           ),
-          ListTile(
-            leading: Icon(Icons.photo_library),
-            title: Text('從相片庫選擇'),
-            onTap: () async {
-              final pickedFile = await picker.pickImage(source: ImageSource.gallery);
-              if (pickedFile != null) {
-                setState(() {
-                  selectedPhoto = pickedFile.path;
-                });
-              }
-              Navigator.pop(context);
-            },
+					Align(
+            alignment: Alignment.centerLeft,          
+            child: Text('商品標題', style: TextStyle(fontSize: 20, color: Colors.white)),
           ),
-        ],
-      ),
-    );
-  }
-
-@override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromRGBO(0xcf, 0xf0, 0x91, 1.0),
-      appBar: AppBar(
-        title: Text('新增商品'),
-        backgroundColor: Color.fromRGBO(0x79, 0xbd, 0x9a, 1.0),
-      ),
-    
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-            child: Column(
-            children: [
-                //
-                SizedBox(height: 20),
-                ElevatedButton(
-                    onPressed: () {
-                        _addProductToDb();
-                    },
-                    child: Text('新增', style: TextStyle(fontSize: 20.0)),
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Color.fromRGBO(0x98, 0xdb, 0x98, 1.0),
-                    ),
+					TextField(
+							maxLength: 50,
+							decoration: InputDecoration(
+								labelText: '商品標題',
+								border: OutlineInputBorder(),
+								),
+						 ),
+					SizedBox(height: 16),
+          const Divider(
+            //height: 20,
+            thickness: 5,
+            //indent: 20,
+            endIndent: 0,
+            color: Color(0xFF4CAD25),
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+					  child: Text('商品描述', style: TextStyle(fontSize: 20, color: Colors.white)),
+          ),
+          TextField(
+							maxLines: 4,
+							maxLength: 200,
+							decoration: InputDecoration(
+								labelText: '商品描述',
+								border: OutlineInputBorder(),
+								),
+						 ),
+					SizedBox(height: 16),
+          const Divider(
+            //height: 20,
+            thickness: 5,
+            //indent: 20,
+            endIndent: 0,
+            color: Color(0xFF4CAD25),
+          ),
+					// 數量的CupertinoPicker
+					Align(
+            alignment: Alignment.centerLeft,
+            child: Text('數量', style: TextStyle(fontSize: 20, color: Colors.white)),
+          ),
+          Container(
+							height: 100.0,
+							child: CupertinoPicker(
+								backgroundColor: Color(0xFF4CAD73),
+								itemExtent: 32.0,
+								onSelectedItemChanged: (int index) {
+								// Implement logic for quantity selection
+							 	},
+                children: List<Widget>.generate(100, (int index) {
+		              return Text('$index', style: TextStyle(color: Colors.white, fontSize: 20.0));
+		            }),
+              ),
+				   ),
+					SizedBox(height: 16),
+          const Divider(
+            //height: 20,
+            thickness: 5,
+            //indent: 20,
+            endIndent: 0,
+            color: Color(0xFF4CAD25),
+          ),
+					// 價格的CupertinoPicker
+          Row(
+            children: <Widget>[
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text('價格', style: TextStyle(fontSize: 20, color: Colors.white)),
+              ),
+              SizedBox(width: 16),  // 提供一些間距
+              Expanded(  // 使用 Expanded 讓 TextField 可以佔用 Row 中剩餘的空間
+                child: TextField(
+                  keyboardType: TextInputType.number,  // 設定鍵盤類型為數字
+                  decoration: InputDecoration(
+                    hintText: '輸入價格',
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(),
+                  ),
                 ),
-                //
-                TextFormField(
-                onChanged: (value) => productName = value,
-                decoration: InputDecoration(
-                    labelText: '商品名稱',
-                ),
-                ),            
-                const SizedBox(height: 16),
-                ElevatedButton(
-                    onPressed: _pickImage,
-                    child: Text('選擇照片', style: TextStyle(fontSize: 20.0)),
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Color.fromRGBO(0x98, 0xdb, 0x98, 1.0),
-                    ),
-                ),
-                if (selectedPhoto != null) ...[
-                    const SizedBox(height: 16),
-                    Image.file(
-                        File(selectedPhoto!),
-                        width: 150,  // 您可以根據需要調整這些值
-                        height: 150,
-                    ),
-                ],                
-                const SizedBox(height: 16),
-                TextFormField(
-                onChanged: (value) => productDescription = value,
-                decoration: InputDecoration(
-                    labelText: '商品描述',
-                    //contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
-                ),
-                ),
-                const SizedBox(height: 16),
-                Text('選擇價格'),
-                SizedBox(height: 10.0),
-                Container(
-                height: 150.0,
-                child: CupertinoPicker(
-                    itemExtent: 30.0,
-                    onSelectedItemChanged: (index) {
-                    setState(() {
-                        selectedPrice = index;
-                    });
-                    },
-                    children: List<Widget>.generate(
-                    1001,
-                    (index) => Text('\$${index.toString()}'),
-                    ),
-                ),
-                ),
-                const SizedBox(height: 16),
-                    Text('選擇數量'),
-                    SizedBox(height: 10.0),
-                    Container(
-                        height: 150.0,
-                        child: CupertinoPicker(
-                            itemExtent: 30.0,
-                            onSelectedItemChanged: (index) {
-                                setState(() {
-                                    selectedQuantity = index;
-                                });
-                            },
-                            children: List<Widget>.generate(
-                                101,
-                                (index) => Text('${index.toString()}'),
-                            ),
-                        ),
-                    ),
-                ],
-            ),
-        ),
-      ),
-    );
-  }
+              ),
+            ],
+          ),
+					SizedBox(height: 16),
+          Align(
+            alignment: Alignment.centerLeft,
+					  child: Text('分類', style: TextStyle(fontSize: 20, color: Colors.white)),
+          ),
+					Container(
+							height: 200.0,
+							child: CupertinoPicker(
+								backgroundColor: Color(0xFF4CAD73),
+								itemExtent: 52.0,
+								onSelectedItemChanged: (int index) {
+								setState(() {
+										dropdownValue = ['macbook pro', 'macbook air', 'ipad', 'iphone'][index];
+										});
+								},
+children: const [
+Text('macbook pro', style: TextStyle(color: Colors.white, fontSize: 32.0)),
+Text('macbook air', style: TextStyle(color: Colors.white, fontSize: 32.0)),
+Text('ipad', style: TextStyle(color: Colors.white, fontSize: 32.0)),
+Text('iphone', style: TextStyle(color: Colors.white, fontSize: 32.0)),
+],
+),
+						 ),
+					],
+					),
+					),
+					),
+					// 儲存與上架按鈕
+					bottomNavigationBar: Row(
+							mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+							children: [
+							Expanded(
+								child: ElevatedButton(
+									style: ElevatedButton.styleFrom(primary: Color(0xFF4CAD73)),
+									child: Text('儲存', style: TextStyle(fontSize: 20)),
+									onPressed: () {
+									// Implement save functionality
+									},
+									),
+								),
+							Expanded(
+								child: ElevatedButton(
+									style: ElevatedButton.styleFrom(primary: Color(0xFF4CAD73)),
+									child: Text('上架', style: TextStyle(fontSize: 20)),
+									onPressed: () {
+									// Implement upload functionality
+									},
+									),
+								),
+							],
+							),
+							);
+		}
 }
